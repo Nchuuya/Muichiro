55384a037ab9864ce5d9a994404f557869e89097
Nchuuya <98148880+Nchuuya@users.noreply.github.com>
2022-02-09 11:26:06 +0530

    Update userinfo.py

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -346,14 +346,12 @@ def info(update: Update, context: CallbackContext):
     if INFOPIC:
         try:
             profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
-            context.bot.sendChatAction(chat.id, "upload_photo")
-            context.bot.send_photo(
-            chat.id,
-            photo=profile,
-            caption=(text),
-            parse_mode=ParseMode.HTML,
-            disable_web_page_preview=True,
-         )
+            _file = bot.get_file(profile["file_id"])
+            _file.download(f"{user.id}.png")
+
+            message.reply_document(
+                document=open(f"{user.id}.png", "rb"),
+                caption=(text),
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
