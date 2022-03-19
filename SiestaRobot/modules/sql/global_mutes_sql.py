1198dc137e653d05154c59f5c9be0bffd9f21819
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-03-19 14:54:28 +0530

    Update global_mutes_sql.py

--- SiestaRobot/modules/sql/global_mutes_sql.py
+++ SiestaRobot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
-from ShasaBot.modules.sql import BASE, SESSION
+from SiestaRobot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
