c38c0051fd902cf131ebc15a06c846ace886f8e1
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-04-07 06:47:31 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -1,24 +1,89 @@
+import asyncio
+import telegram
 import os
-import re
-from platform import python_version as kontol
-from telethon import events, Button
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
+import requests
+import datetime
+import time
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import random
+from telethon import events, Button, custom, version
 from SiestaRobot.events import register
-from SiestaRobot import telethn as tbot
+from SiestaRobot import telethn as borg, OWNER_ID, OWNER_NAME
+from SiestaRobot import StartTime, dispatcher
+from telethon.tl.types import ChannelParticipantsAdmins
+from pyrogram import __version__ as pyro
 
 
-PHOTO = "https://telegra.ph/file/6c63db45cd26d0a711da8.mp4"
+edit_time = 5
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/f1b6b36af5b4dc85e67e3.jpg"
+file2 = "https://telegra.ph/file/6553f53687ed05f74027e.jpg"
+file3 = "https://telegra.ph/file/41bb289e29fd23df314cb.jpg"
+file4 = "https://telegra.ph/file/644c04a3c809a83d73df3.jpg"
+""" =======================CONSTANTS====================== """
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
 
 @register(pattern=("/alive"))
-async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Boa Hancock, Nice to meet you you are my Luffy💖💜** \n\n"
-  TEXT += "🐍 **I'm Working Properly** \n\n"
-  TEXT += f"🐍 **My Master : [Kazutora Hanemiya](https://t.me/zerohisoka)** \n\n"
-  TEXT += f"🐍 **Library Version :** `{telever}` \n\n"
-  TEXT += f"🐍 **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"🐍 **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/BoaHancock_Robot?start=help"), Button.url("ᴀɴɪᴍᴇ ᴄʜᴀᴛ", "https://t.me/straydogs")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+async def hmm(yes):
+    chat = await yes.get_chat()
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    Miku = f"♡ **Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}), I'm Boa Hancock ♡**\n\n"
+    Miku += f"♡ **My Uptime** ~♪ `{uptime}`\n\n"
+    Miku += f"♡ **Telethon Version** ~♪ `{version.__version__}`\n\n"
+    Miku += f"♡ **Python Telegram Bot Version** ~♪ `{telegram.__version__}`\n\n"
+    Miku += f"♡ **Pyrogram Version** ~♪ `{pyro}`\n\n"
+    Miku += f"♡ **My Master** ~♪ [{OWNER_NAME}](tg://user?id={OWNER_ID})\n\n"
+    Miku += f"Thanks For Adding Me In {yes.chat.title}"
+    BUTTON = [[Button.url("Support Chat", "https://t.me/BoaHancock_Support"), Button.url("Updates Channel", "https://t.me/boa_updates")]]
+    on = await borg.send_file(yes.chat_id, file=file2,caption=Miku, buttons=BUTTON)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(yes.chat_id, on, file=file3, buttons=BUTTON) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=file4, buttons=BUTTON)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1, buttons=BUTTON)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file2, buttons=BUTTON)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file1, buttons=BUTTON)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file3, buttons=BUTTON)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4, buttons=BUTTON)
+
+@register(pattern=("/repo"))
+async def repo(event):
+    Miku = f"**Hey [{event.sender.first_name}](tg://user?id={event.sender.id}), Click The Button Below To Get My Yamato Repo**\n\n"
+    BUTTON = [[Button.url("[► Support ◄]", "https://t.me/BoaHancock_Support"), Button.url("[► Repo ◄]", "https://github.com/Nchuuya/Yamato")]]
+    await borg.send_file(event.chat_id, file="https://telegra.ph/file/41bb289e29fd23df314cb.jpg", caption=Miku, buttons=BUTTON
