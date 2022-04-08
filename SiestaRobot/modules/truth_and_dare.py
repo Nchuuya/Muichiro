780bed796b49e088b9f830bbdbc26e71347c289e
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-04-08 07:11:51 +0530

    Update truth_and_dare.py

--- SiestaRobot/modules/truth_and_dare.py
+++ SiestaRobot/modules/truth_and_dare.py
@@ -16,9 +16,38 @@ def dare(update: Update, context: CallbackContext):
     args = context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
 
+def sigma(update: Update, context: CallbackContext):
+    update.effective_message.reply_video(random.choice(truth_and_dare_string.SIGMA))
+
+
+def cosplay(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(random.choice(truth_and_dare_string.COSPLAY))
+
+def onepiece(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(random.choice(truth_and_dare_string.WAIFU))
+
+def naruto(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(random.choice(truth_and_dare_string.NARUTO))
+
+
+
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
+SIGMA_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
+COSPLAY_HANDLER = DisableAbleCommandHandler("cosplay", cosplay, run_async=True)
+WAIFU_HANDLER = DisableAbleCommandHandler("onepiece", onepiece, run_async=True)
+YAMATO_HANDLER = DisableAbleCommandHandler("yamato", yamato, run_async=True)
+NARUTO_HANDLER = DisableAbleCommandHandler("naruto", naruto, run_async=True)
+
+
+
+
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
+dispatcher.add_handler(SIGMA_HANDLER)
+dispatcher.add_handler(COSPLAY_HANDLER)
+dispatcher.add_handler(WAIFU_HANDLER)
+dispatcher.add_handler(YAMATO_HANDLER)
+dispatcher.add_handler(NARUTO_HANDLER)
