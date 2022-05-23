e3098c42de9975714158824d8bae1db99ea7cb48
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-23 20:05:59 +0530

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -98,6 +98,7 @@ if ENV:
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    REDIS_URL = os.environ.get("REDIS_URL", None) 
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
