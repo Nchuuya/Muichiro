4c517f5d40389dc9ec8263b920de18645ecb9b79
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-24 12:00:51 +0530

    Update chat_status.py

--- SiestaRobot/modules/helper_funcs/chat_status.py
+++ SiestaRobot/modules/helper_funcs/chat_status.py
@@ -375,6 +375,10 @@ def user_can_ban(func):
 
     return user_is_banhammer
 
+def callbacks_in_filters(data):
+    return filters.create(lambda flt, _, query: flt.data in query.data, data=data)
+
+
 
 def connection_status(func):
     @wraps(func)
