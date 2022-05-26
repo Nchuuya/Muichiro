8dd78f2edee07b15ffe46fe1b5c61c6905433a35
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-26 08:43:43 +0530

    Update rules.py

--- SiestaRobot/modules/rules.py
+++ SiestaRobot/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import TGN.modules.sql.rules_sql as sql
-from TGN import dispatcher
-from TGN.modules.helper_funcs.chat_status import user_admin
-from TGN.modules.helper_funcs.string_handling import markdown_parser
+import SiestaRobot.modules.sql.rules_sql as sql
+from SiestaRobot import dispatcher
+from SiestaRobot.modules.helper_funcs.chat_status import user_admin
+from SiestaRobot.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
