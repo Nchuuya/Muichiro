5192f2c91ed330948e2c18f3430cc8c8154e5127
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-29 21:21:25 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,11 +9,14 @@ from pyrogram import __version__ as pyrover
 from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/0bd997f802acca9cd8953.mp4"
+PHOTO = "https://telegra.ph/file/1a73924778901c994e1c1.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ᴋᴏɴɪɴᴄʜɪᴡᴀ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ɴᴀᴍɪ♡ ɪ ᴀᴍ ᴡᴏʀᴋɪɴɢ ᴘᴇʀꜰᴇᴄᴛʟʏ**\nɪ ᴀᴍ ʜᴇʀᴇ ᴛᴏ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ꜱᴏ ɢɪᴠᴇ ᴍᴇ ᴇɴᴏᴜɢʜ ʀɪɢʜᴛꜱ\nꜰᴏʀ ᴀɴʏ ᴘʀᴏʙʟᴇᴍꜱ ɪɴ ʙᴏᴛ ᴀꜱᴋ ᴍʏ ᴅᴇᴠꜱ - @zerohisooka\n\n"
+  TEXT += "♡ **I'm Working Properly** \n\n"
+  TEXT += f"♡ **Library Version :** `{telever}` \n\n"
+  TEXT += f"♡ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += "**Thanks For Adding Me Here ♡**"
   BUTTON = [[Button.url("Help", "https://t.me/namiirobot?start=help"), Button.url("Support", "https://t.me/boahancock_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
