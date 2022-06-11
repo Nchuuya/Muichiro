449cadd299111ed68907e2022e9a73dcc374290a
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-06-11 10:59:42 +0530

    Update script.py

--- SiestaRobot/script.py
+++ SiestaRobot/script.py
@@ -1,13 +1,12 @@
 MIKU_IMG = (
-      "https://telegra.ph/file/91fe4f36e0dc0de5ad2eb.jpg",
-      "https://telegra.ph/file/9dfd1212b7dc0fc6b390b.jpg",
-      "https://telegra.ph/file/0167f11085e44f8bb8042.jpg",
-      "https://telegra.ph/file/4206f387f53a5d3be0622.jpg",
-      "https://telegra.ph/file/42830b5cb88ca6b129e71.jpg",
+      "https://telegra.ph/file/bcd9adfe3bc37e6586bad.jpg",
+      "https://telegra.ph/file/fb23b5c579df06a05f19d.jpg",
+      "https://telegra.ph/file/8ddbc0dd32af976e44e42.jpg",
+      
 )
 
 PM_PHOTO = (
-      "https://telegra.ph/file/42830b5cb88ca6b129e71.jpg",
-      "https://telegra.ph/file/91fe4f36e0dc0de5ad2eb.jpg",
-      "https://telegra.ph/file/0167f11085e44f8bb8042.jpg",
+      "https://telegra.ph/file/bcd9adfe3bc37e6586bad.jpg",
+      "https://telegra.ph/file/fb23b5c579df06a05f19d.jpg",
+      "https://telegra.ph/file/8ddbc0dd32af976e44e42.jpg",
 )
